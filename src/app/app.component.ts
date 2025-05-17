a6c2b52e13b63ace9dde3035d0bd99cb680b7fdb
RandNassar02 <nassarrand75@gmail.com>
2025-05-17 21:16:46 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe } from './i18n/translate.pipe';
+import { I18nService } from './i18n/i18n.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'delivery-management-system';
+  constructor(private i18nService: I18nService) {}
+
+  ngOnInit() {
+    this.i18nService.loadTranslations('en');
+  }
 }
